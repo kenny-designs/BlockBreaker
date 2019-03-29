d93e4d86be153d6313eae44e4e92da7b54fe3fcd
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-03-28 23:07:18 -0400

    First commit

---
+++ Block Breaker/Assets/Scripts/GameSession.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour {
+  // config params
+  [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+  [SerializeField] int pointsPerBlockDestroyed = 83;
+  [SerializeField] TextMeshProUGUI scoreText;
+  [SerializeField] bool isAutoPlayEnabled = false;
+
+  // state variables
+  [SerializeField] int currentScore = 0;
+
+  private void Awake() {
+    int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+
+    // check if an instance of GameStatus already exists as part of our singleton pattern
+    if (gameStatusCount > 1) {
+      // always SetActive to false for the singleton pattern. Just destroying isn't enough
+      // for there may exist a split moment during the update before the GameStatus object
+      // is actually destroyed as per the script lifecycle
+      gameObject.SetActive(false);
+      Destroy(gameObject);
+    }
+    else {
+      DontDestroyOnLoad(gameObject);
+    }
+  }
+
+  private void Start() {
+    scoreText.text = currentScore.ToString();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    Time.timeScale = gameSpeed;
+  }
+
+  public void AddToScore() {
+    currentScore += pointsPerBlockDestroyed;
+    scoreText.text = currentScore.ToString();
+  }
+
+  public void ResetGame() {
+    Destroy(gameObject);
+  }
+
+  public bool IsAutoPlayEnabled() {
+    return isAutoPlayEnabled;
+  }
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+  // configuration parameters
+  [SerializeField] float minX = 1f, maxX = 15f;
+  [SerializeField] float screenWidthInUnits = 16f;
+
+  // cached component references
+  GameSession theGameSession;
+  Ball theBall;
+
+  private void Start() {
+    theGameSession = FindObjectOfType<GameSession>();
+    theBall = FindObjectOfType<Ball>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+    paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+    transform.position = paddlePos;
+  }
+
+  private float GetXPos() {
+    if (theGameSession.IsAutoPlayEnabled()) {
+      return theBall.transform.position.x;
+    }
+
+    return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+  }
+}
